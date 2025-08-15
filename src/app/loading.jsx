d206b1f60d4f70335e05e9fd218a4df7241a6f3a
Md Shahjalal <muhommodshahjalal@gmail.com>
2025-08-15 16:52:11 +0600

    feat: Added public/serviceData.json | refactor: Updated app/loading.jsx, [id]/page.jsx

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -2,12 +2,12 @@ const Loading = () => {
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div>
-        <h2>loading...</h2>
-        <span className="loading loading-ball loading-xs"></span>
-        <span className="loading loading-ball loading-sm"></span>
-        <span className="loading loading-ball loading-md"></span>
-        <span className="loading loading-ball loading-lg"></span>
-        <span className="loading loading-ball loading-xl"></span>
+        <h2 className="text-center">Loading...</h2>
+        <span className="loading loading-bars loading-xs"></span>
+        <span className="loading loading-bars loading-sm"></span>
+        <span className="loading loading-bars loading-md"></span>
+        <span className="loading loading-bars loading-lg"></span>
+        <span className="loading loading-bars loading-xl"></span>
       </div>
     </div>
   );
--- src/app/services/[id]/page.jsx
+++ src/app/services/[id]/page.jsx
@@ -1,10 +1,17 @@
+import dbConnect, { collectionsNameObj } from "@/lib/dbConnect";
+
 const ServicesDetails = async ({ params }) => {
   const p = await params;
+
+  const serviceCollection = dbConnect(collectionsNameObj.servicesCollection);
   console.log(p.id, "dynamicId in params", 3);
+
+  const datas = serviceCollection.findOne({ _id: p.id });
 
   return (
     <div>
       <h2>ServicesDetails</h2>
+      {JSON.stringify(datas)}
     </div>
   );
 };

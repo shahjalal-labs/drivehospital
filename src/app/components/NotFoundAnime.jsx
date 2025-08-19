3dd0268310eef152a5d6f7c204d8febc1264d323
Md Shahjalal <muhommodshahjalal@gmail.com>
2025-08-19 16:14:18 +0600

    refactor: Updated components/NotFoundAnime.jsx, app/not-found.jsx

--- src/app/components/NotFoundAnime.jsx
+++ src/app/components/NotFoundAnime.jsx
@@ -4,8 +4,21 @@ import LottieAnime from "@/lib/LottieAnime";
 
 const NotFoundAnime = () => {
   return (
-    <div>
-      <LottieAnime anime={notFoundLottie} />
+    <div className="mb-10">
+      <div className="max-w-[900px] mx-auto">
+        <LottieAnime anime={notFoundLottie} />
+      </div>
+      <div className="flex flex-col items-center justify-center  -mt-24">
+        <p className="mt-6 text-lg text-gray-400">
+          The page you're looking for doesn’t exist.
+        </p>
+        <Link
+          href="/"
+          className="btn btn-info btn-outline mt-3 text-lg font-semibold rounded-full"
+        >
+          Go Back Home
+        </Link>
+      </div>
     </div>
   );
 };

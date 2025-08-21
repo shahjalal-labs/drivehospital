a14779aa442e23a735f3b1d388e654933e145f4e
Md Shahjalal <muhommodshahjalal@gmail.com>
2025-08-21 17:28:42 +0600

    refactor: Updated auth/loginUser.js

--- src/app/actions/auth/loginUser.js
+++ src/app/actions/auth/loginUser.js
@@ -1,1 +1,20 @@
-export const loginUser = (payload) => {};
+"use server";
+import dbConnect, { collectionsNameObj } from "@/lib/dbConnect";
+import bcrypt from "bcrypt";
+
+export const loginUser = async (payload) => {
+  const userCollection = dbConnect(collectionsNameObj.usersCollection);
+
+  const { email, password } = payload;
+  if (!email || !password) return null;
+
+  const user = await userCollection.findOne({ email });
+  if (!user) return null;
+
+  // const isPasswordOK = await bcrypt.compare(user.password, password);
+  const isPasswordOK = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordOK) return null;
+
+  return user;
+};

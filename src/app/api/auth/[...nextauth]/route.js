4b3ab112763a8faeb3601235934a9c990e537cab
Md Shahjalal <muhommodshahjalal@gmail.com>
2025-08-21 15:36:42 +0600

    feat: Added lib/authOptions.js | refactor: Updated [...nextauth]/route.js

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,16 +1,8 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
+import { authOptions } from "@/lib/authOptions";
 
-export const authOptions = {
-  // Configure one or more authentication providers
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-    GoogleProvider({}),
-  ],
-};
+const handler = NextAuth({
+  authOptions,
+});
 
-export default NextAuth(authOptions);
+export { handler as GET, handler as POST };
